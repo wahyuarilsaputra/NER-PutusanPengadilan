8820f1efe415687d4630d71c7ebc6f2012857032
Putraa <90175053+wahyuarilsaputra@users.noreply.github.com>
2024-07-14 23:27:05 +0700

    Coba Heroku

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def implementasi():
     return render_template('implementasi.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
